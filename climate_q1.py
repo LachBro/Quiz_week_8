1177e04636bd815e47b1440e1433ca776a7633a8
LachBro <140813114+LachBro@users.noreply.github.com>
2023-09-16 12:41:52 +1000

    climate_q1_FINAL ANSWER.py
    with insert query

--- climate_q1.py
+++ climate_q1.py
@@ -4,6 +4,25 @@ import sqlite3
 connection = sqlite3.connect('climate.db')
 cursor = connection.cursor()
 
+#values to be inserted
+insert_data = """
+    INSERT INTO ClimateData (Year, CO2, Teamperature)
+    VALUES
+    (1950, 250, 14.1),
+    (1960, 265, 15.5),
+    (1970, 272, 16.3),
+    (1980, 260, 18.1),
+    (1990, 300, 17.3),
+    (2000, 320, 19.1),
+    (2010, 389, 20.2)
+"""
+
+#execute the query to insert values
+cursor.execute(insert_data)
+
+#commit changes to climate.db database
+connection.commit()
+
 # retrieve data from climate.db database
 query = "SELECT Year, CO2, Temperature FROM ClimateData"
 cursor.execute(query)
@@ -29,5 +48,4 @@ plt.xlabel("Year (decade)")
 plt.savefig("co2_temp_1.png")
 plt.show()
 
-#close the database connection
 connection.close()

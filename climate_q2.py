b3f6d2be811b2442aa6cc16b552dc3225bf1fc56
LachBro <140813114+LachBro@users.noreply.github.com>
2023-09-16 11:30:20 +1000

    climate_q2_Answered.py

--- climate_q2.py
+++ climate_q2.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+df = pd.read_csv('climate.csv') #load the datafile climate.csv using pandas
 
+#get data from data frame
+years = df['Year']
+co2 = df['CO2']
+temp = df['Temperature']
+
+#create subplots
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
